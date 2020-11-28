e17d1481afde32a81629ce5b72d82d949dfd38c9
etasdemir <erentasdemircs@gmail.com>
2020-11-28 20:53:19 +0300

    Add Game Activity

---
+++ app/src/main/java/com/h5170043/muhammed_cagatay_mercan/ui/lost/GameLostActivity.java
@@ -0,0 +1,16 @@
+package com.h5170043.muhammed_cagatay_mercan.ui.lost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.h5170043.muhammed_cagatay_mercan.R;
+
+public class GameLostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_lost);
+    }
+}
---
+++ app/src/main/java/com/h5170043/muhammed_cagatay_mercan/ui/won/GameWonActivity.java
@@ -0,0 +1,16 @@
+package com.h5170043.muhammed_cagatay_mercan.ui.won;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.h5170043.muhammed_cagatay_mercan.R;
+
+public class GameWonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_won);
+    }
+}

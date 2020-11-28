a5267f546672dd1e5ceca875adc017b29710a262
etasdemir <erentasdemircs@gmail.com>
2020-11-28 13:50:27 +0300

    Init project

---
+++ app/src/main/java/com/h5170043/muhammed_cagatay_mercan/QuizApplication.java
@@ -0,0 +1,10 @@
+package com.h5170043.muhammed_cagatay_mercan;
+
+import android.app.Application;
+
+public class QuizApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
